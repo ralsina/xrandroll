4f27512780b28aa5cd64c2daaea6798b55d987ff
Roberto Alsina <ralsina@netmanagers.com.ar>
2020-02-05 17:34:46 -0300

    Some more tests

---
+++ tests/conftest.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+from fixtures import TestData
+
+TestData.BASE_PATH = Path(__file__).parent / "fixtures"
--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -1,10 +1,22 @@
 from xrandroll.monitor import Monitor
 
+BASIC_HEADER = "eDP connected primary 1920x1080+0+1080 (0x56) normal (normal left inverted right x axis y axis) 309mm x 173mm"  # noqa
+
 
 def test_parse_pos():
-    data = [
-        "eDP connected primary 1920x1080+0+1080 (0x56) normal (normal left inverted right x axis y axis) 309mm x 173mm"
-    ]
+    data = [BASIC_HEADER]
     m = Monitor(data)
     assert m.pos_x == 0
     assert m.pos_y == 1080
+
+
+def test_parse_output():
+    data = [BASIC_HEADER]
+    m = Monitor(data)
+    assert m.output == "eDP"
+
+
+def test_parse_modes(test_data):
+    data = test_data.read("monitor_1.txt", deserialize=False).splitlines()
+    m = Monitor(data)
+    assert len(m.modes) == 9
